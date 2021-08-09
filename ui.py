ff5b568546dba8315cc96204bca6f700d3b45bd9
Amit Samanta <developer.amitsamanta@gmail.com>
2021-08-09 10:11:43 +0530

    Add command functionality for the buttons

--- ui.py
+++ ui.py
@@ -28,11 +28,19 @@ class QuizInterface:
         self.canvas.grid(column=0, row=1, columnspan=2, pady=25)
 
         true_image = PhotoImage(file="./images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(
+            image=true_image,
+            highlightthickness=0,
+            command=self.true_pressed
+        )
         self.true_button.grid(column=0, row=2)
 
         false_image = PhotoImage(file="./images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(
+            image=false_image,
+            highlightthickness=0,
+            command=self.false_pressed
+        )
         self.false_button.grid(column=1, row=2)
 
         self.get_next_question()
@@ -42,3 +50,9 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_pressed(self):
+        self.quiz.check_answer("True")
+
+    def false_pressed(self):
+        self.quiz.check_answer("False")
