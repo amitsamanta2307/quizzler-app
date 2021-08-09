0589e8f04d8b40fc7a551598616db0e70d4e686c
Amit Samanta <developer.amitsamanta@gmail.com>
2021-08-09 09:30:17 +0530

    Create UI for the quizzler app using tkinter

--- ui.py
+++ ui.py
@@ -1,2 +1,34 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=50, pady=50, bg=THEME_COLOR)
+
+        self.score_label = Label(text=f"Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(height=250, width=300, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="question",
+            fill=THEME_COLOR,
+            font=("Ariel", 20, "italic")
+        )
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=25)
+
+        true_image = PhotoImage(file="./images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_image = PhotoImage(file="./images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
